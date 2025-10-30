6bbf88563242bd5fd577de1904a5f88cb3c453d9
aomalley2025 <orders@rationsandrootsmeal.com>
2025-10-30 13:07:40 -0500

    Fix Stripe import for Netlify CommonJS runtime

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,5 +1,6 @@
 // netlify/functions/create-checkout-session.js
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
   try {
